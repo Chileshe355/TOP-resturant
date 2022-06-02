34b67915a2e1cca8e394651c80283b439fb4a7af
Chileshe355 <chileshe355@gmail.com>
2022-06-02 15:41:40 +0200

    basic home page in html and working and linked webpack css

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # TOP-resturant
+
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import _ from 'lodash';
+import './style.css';
+import pic from './background.jpeg';
+
+const bgPic =new Image();
+bgPic.src=pic;
+
+// function component() {
+//     const element = document.createElement('div');
+  
+//     // Lodash, currently included via a script, is required for this line to work
+//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
+//     return element;
+//   }
+  
+//   document.body.appendChild(component());
+  
